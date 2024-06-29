2f297e672fd90dde79344f490f453cd2480f6544
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-06-29 15:15:22 +0530

    React components and dynamic data lesson

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,22 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max+1));
+}
+
+
 function Header(){
+
+  const desc = reactDescriptions[getRandomInt(reactDescriptions.length) - 1];
+  console.log(getRandomInt(reactDescriptions.length - 1));
+  console.log(desc);
+
   return(
   <header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      {desc} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
