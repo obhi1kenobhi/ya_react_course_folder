a727771feb5da77907ec7fc3c942e204c15a98a4
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-06-29 15:46:10 +0530

    Reusable components, props and using the spread operator for optimization of props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,6 @@
+import reactImage from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max) {
@@ -13,7 +16,7 @@ function Header(){
 
   return(
   <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactImage} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       {desc} React concepts you will need for almost any app you are
@@ -23,11 +26,30 @@ function Header(){
   );
 }
 
+function CoreConcept(props) {
+  return(
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
